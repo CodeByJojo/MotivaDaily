d9696c85f11ad12027941a710f15273eeeead831
Joe Maltba <joe.maltba@gmail.com>
2023-04-19 10:13:08 -0400

    NASA API Done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// NASA PIC API
+fetch('https://api.nasa.gov/planetary/apod?api_key=zfHhKMgvbXkwzU4eQ3nAMQmz5K9X0AuHU8ve0Wul')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.hdurl)
+        document.getElementById('thePic').src = data.hdurl
+        
+    })
